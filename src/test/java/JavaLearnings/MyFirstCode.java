f502b863c13bef8ffacc6d914f00b765cd43c11c
AshwiniNelge <ashwininelge41@gmail.com>
2023-08-18 14:34:07 +0530

    First time adding project to git

---
+++ src/test/java/JavaLearnings/MyFirstCode.java
@@ -0,0 +1,10 @@
+package JavaLearnings;
+
+public class MyFirstCode {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hey my first ever java code in vision it training ");
+	}
+	
+}
